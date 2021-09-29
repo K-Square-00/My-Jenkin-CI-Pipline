3cce35727599041ecf347e27fbaf506736478637
Zhou Kankan <kan.kan.zhou@outlook.com>
2021-09-29 09:20:51 +0800

    add testcase for age

--- src/test_app.py
+++ src/test_app.py
@@ -5,6 +5,10 @@ class testStudent(unittest.TestCase):
     def test_get_name(self):
         jack = Student("jack",18)
         self.assertEqual(jack.get_name(), "jack")
+    
+    def test_get_age(self):
+        jack = Student("jack",18)
+        self.assertEqual(jack.get_age(),18)
 
 
 
